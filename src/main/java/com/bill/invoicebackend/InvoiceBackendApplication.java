b2725053f9741efc95f8e8fbb37ddf2a5f5c1493
nishi90297 <nishi.90297@gmail.com>
2020-07-23 16:46:16 +0530

    initalizing invoice backend project

---
+++ src/main/java/com/bill/invoicebackend/InvoiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bill.invoicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bill/invoicebackend/InvoiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.invoicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
