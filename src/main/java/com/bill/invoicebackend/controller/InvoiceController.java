984cea1c36c313ccdb521f503150c64aba3e1f85
Nishtha Garg <nishi.90297@gmail.com>
2020-08-24 01:06:28 +0530

    added getAllInvoice api

--- src/main/java/com/bill/invoicebackend/controller/InvoiceController.java
+++ src/main/java/com/bill/invoicebackend/controller/InvoiceController.java
@@ -3,9 +3,12 @@ package com.bill.invoicebackend.controller;
 import com.bill.invoicebackend.model.Invoice;
 import com.bill.invoicebackend.respository.InvoiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class InvoiceController {
@@ -18,5 +21,12 @@ public class InvoiceController {
         invoiceRepository.save(invoice);
         return "Successfully Added";
     }
+
+
+    @GetMapping(value = "getAllInvoices")
+    public List<Invoice> getAllInvoices(){
+        return invoiceRepository.findAll();
+    }
+
 
 }
