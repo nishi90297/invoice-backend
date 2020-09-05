4aac530c27d000dea1cff29e28c959cfbca32cc4
Nishtha Garg <nishi.90297@gmail.com>
2020-09-05 15:13:03 +0530

    removed invoice data

--- src/main/java/com/bill/invoicebackend/controller/InvoiceController.java
+++ src/main/java/com/bill/invoicebackend/controller/InvoiceController.java
@@ -3,10 +3,13 @@ package com.bill.invoicebackend.controller;
 import com.bill.invoicebackend.model.Biller.Biller;
 import com.bill.invoicebackend.model.Invoice.Invoice;
 import com.bill.invoicebackend.utils.FilePath;
+import com.bill.invoicebackend.utils.enums.FileTypes;
 import com.bill.invoicebackend.utils.enums.InvoiceStatus;
 import com.bill.invoicebackend.respository.BillerRepository;
 import com.bill.invoicebackend.respository.InvoiceRepository;
 import com.bill.invoicebackend.utils.Util;
+import com.github.jhonnymertz.wkhtmltopdf.wrapper.Pdf;
+import com.github.jhonnymertz.wkhtmltopdf.wrapper.configurations.WrapperConfig;
 import org.htmlcleaner.CleanerProperties;
 import org.htmlcleaner.HtmlCleaner;
 import org.htmlcleaner.PrettyXmlSerializer;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.util.List;
 
 @RestController
@@ -38,18 +42,6 @@ public class InvoiceController {
         return billerRepository.findByEmail(authentication.getName());
     }
 
-    @PostMapping(value = "addInvoiceForm")
-    public String addInvoiceForm(@RequestBody Invoice invoice){
-        invoice.setCreatedBy(getBillerInfo().getId());
-        invoice.setCreatedAt();
-        invoice.setStatus(InvoiceStatus.DATA_SAVED);
-        invoice.setInvoiceNumber("INV-"+(getInvoiceNumber()-1));
-        System.out.println(invoice.toString());
-        invoiceRepository.save(invoice);
-
-        return invoiceRepository.save(invoice).getId();
-    }
-
     @GetMapping(value = "getAllInvoices")
     public List<Invoice> getAllInvoices(){
         return invoiceRepository.findByCreatedBy(getBillerInfo().getId());
@@ -66,10 +58,32 @@ public class InvoiceController {
         return invoiceList.size()+1;
     }
 
-    @GetMapping(value = "testAPI")
-    public static void test(){
+    @PostMapping(value = "addInvoiceForm")
+    public String addInvoiceForm(@RequestBody Invoice invoice){
+        invoice.setCreatedBy(getBillerInfo().getId());
+        invoice.setCreatedAt(Instant.now());
+        invoice.setInvoiceStatus(InvoiceStatus.DATA_SAVED);
+        System.out.println(invoice.toString());
+        invoiceRepository.save(invoice);
+        generatePDF(invoice);
+        return invoiceRepository.save(invoice).getId();
+    }
+
+    @GetMapping(value = "generatePDF")
+    public void generatePDF(Invoice invoice){
         try{
-            String str = Util.convertFileToString("Template/invoices/invoice1.html").replace("$payerName$","Nishtha");
+            String fileStr = Util.convertFileToString("Template/invoices/invoice1.html")
+                    .replace("$payerName$",invoice.getPayerName())
+                    .replace("$payerAddress$",invoice.getPayerAddress())
+                    .replace("$payerEmail$",invoice.getPayerEmail())
+                    .replace("$invoiceNumber$",invoice.getInvoiceNumber())
+                    .replace("$createdDate$",invoice.getCreatedAt().toString())
+                    .replace("$dueDate$",invoice.getDueDate().toString())
+                    .replace("$total$",invoice.getTotalAmount().toString())
+                    .replace("$discount$",invoice.getDiscount().toString())
+                    .replace("$discountedAmount$",invoice.getDiscountedAmount().toString())
+                    .replace("$finalAmount$",invoice.getFinalAmount().toString());
+
 
             //clean up html to render html properly.Will show error if any open tag or any issue.
             HtmlCleaner htmlCleaner = new HtmlCleaner();
@@ -81,9 +95,9 @@ public class InvoiceController {
             cleanerProperties.setOmitComments(true);
 
             //tagnode contains clean html code
-            TagNode cleanedHtmlNode=htmlCleaner.clean(str);
+            TagNode cleanedHtmlNode=htmlCleaner.clean(fileStr);
 
-            FilePath path = new FilePath("123");
+            FilePath path = new FilePath(getBillerInfo().getId());
 
             File file = new File(path.getFileUploadDirectory()+path.generateSubFolderPath());
             Boolean folderExists = false;
@@ -98,6 +112,12 @@ public class InvoiceController {
                 PrettyXmlSerializer prettyXmlSerializer = new PrettyXmlSerializer(cleanerProperties);
                 prettyXmlSerializer.writeToFile(cleanedHtmlNode,tempFileFullPath, StandardCharsets.UTF_8.name());
             }
+
+            WrapperConfig wrapperConfig = new WrapperConfig(WrapperConfig.findExecutable());
+            Pdf pdf = new Pdf(wrapperConfig);
+            pdf.addPageFromFile(path.getFullPath());
+            path.setFileTypes(FileTypes.PDF);
+            pdf.saveAs("/home/nishtha/Work/invoice-backend/OuputPdfs/"+path.getSubFolderPath()+path.getFileName());
         }
         catch (Exception e){
             e.printStackTrace();
--- src/main/java/com/bill/invoicebackend/model/Invoice/Invoice.java
+++ src/main/java/com/bill/invoicebackend/model/Invoice/Invoice.java
@@ -2,35 +2,41 @@ package com.bill.invoicebackend.model.Invoice;
 
 import com.bill.invoicebackend.utils.enums.InvoiceStatus;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
+import java.util.List;
 
 @Document(value = "Invoice")
 public class Invoice {
 
     @Id
     private String id;
-    private Instant createdAt;
-    private String createdBy;
-    private Double total;
-    private InvoiceStatus invoiceStatus;
-    private Integer statusCode;
-    private InvoiceData invoiceData;
+    private Number templateNo;
+
     private String invoiceNumber;
 
-    @Override
-    public String toString() {
-        return "Invoice{" +
-                "id='" + id + '\'' +
-                ", createdAt=" + createdAt +
-                ", createdBy='" + createdBy + '\'' +
-                ", total=" + total +
-                ", invoiceStatus=" + invoiceStatus +
-                ", statusCode=" + statusCode +
-                ", invoiceData=" + invoiceData +
-                '}';
-    }
+    //set in controller
+    private Instant createdAt;
+    private String createdBy;
+    private InvoiceStatus invoiceStatus;
+    private Integer statusCode;
+
+    private String payerName;
+    @Indexed(unique=true)
+    private String payerEmail;
+    private String payerAddress;
+    private List<InvoiceProduct> products;
+
+    private Instant dueDate;
+    private String freeText;
+    private String footer;
+    private Number discount;
+    private Double totalAmount;
+    private Double discountedAmount;
+    private Double finalAmount;
+
 
     public String getId() {
         return id;
@@ -40,12 +46,20 @@ public class Invoice {
         this.id = id;
     }
 
+    public Number getTemplateNo() {
+        return templateNo;
+    }
+
+    public void setTemplateNo(Number templateNo) {
+        this.templateNo = templateNo;
+    }
+
     public Instant getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt() {
-        this.createdAt = Instant.now();
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
     }
 
     public String getCreatedBy() {
@@ -56,21 +70,21 @@ public class Invoice {
         this.createdBy = createdBy;
     }
 
-    public Double getTotal() {
-        return total;
+    public String getInvoiceNumber() {
+        return invoiceNumber;
     }
 
-    public void setTotal(Double total) {
-        this.total = total;
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
     }
 
-    public InvoiceStatus getStatus() {
+    public InvoiceStatus getInvoiceStatus() {
         return invoiceStatus;
     }
 
-    public void setStatus(InvoiceStatus status) {
-        this.invoiceStatus = status;
-        this.statusCode = invoiceStatus.getStatusCode();
+    public void setInvoiceStatus(InvoiceStatus invoiceStatus) {
+        this.invoiceStatus = invoiceStatus;
+        this.setStatusCode(invoiceStatus.getStatusCode());
     }
 
     public Integer getStatusCode() {
@@ -81,31 +95,91 @@ public class Invoice {
         this.statusCode = statusCode;
     }
 
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
+    public String getPayerName() {
+        return payerName;
     }
 
-    public InvoiceStatus getInvoiceStatus() {
-        return invoiceStatus;
+    public void setPayerName(String payerName) {
+        this.payerName = payerName;
     }
 
-    public void setInvoiceStatus(InvoiceStatus invoiceStatus) {
-        this.invoiceStatus = invoiceStatus;
+    public String getPayerEmail() {
+        return payerEmail;
     }
 
-    public InvoiceData getInvoiceData() {
-        return invoiceData;
+    public void setPayerEmail(String payerEmail) {
+        this.payerEmail = payerEmail;
     }
 
-    public void setInvoiceData(InvoiceData invoiceData) {
-        this.invoiceData = invoiceData;
+    public List<InvoiceProduct> getProducts() {
+        return products;
     }
 
-    public String getInvoiceNumber() {
-        return invoiceNumber;
+    public void setProducts(List<InvoiceProduct> products) {
+        this.products = products;
     }
 
-    public void setInvoiceNumber(String invoiceNumber) {
-        this.invoiceNumber = invoiceNumber;
+    public Instant getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Instant dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getFreeText() {
+        return freeText;
+    }
+
+    public void setFreeText(String freeText) {
+        this.freeText = freeText;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+    }
+
+    public Number getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Number discount) {
+        this.discount = discount;
+    }
+
+    public String getPayerAddress() {
+        return payerAddress;
+    }
+
+    public void setPayerAddress(String payerAddress) {
+        this.payerAddress = payerAddress;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Double getDiscountedAmount() {
+        return discountedAmount;
+    }
+
+    public void setDiscountedAmount(Double discountedAmount) {
+        this.discountedAmount = discountedAmount;
+    }
+
+    public Double getFinalAmount() {
+        return finalAmount;
+    }
+
+    public void setFinalAmount(Double finalAmount) {
+        this.finalAmount = finalAmount;
     }
 }
